0e50a818c9ca73507bf3da6b0a3887cff312fbd1
mustafakemal1729 <experimus1@gmail.com>
2023-06-02 23:44:23 +0200

    caesar cipher

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,5 +1,16 @@
-def caesar_cipher(text: str, shift: int) -> str:
-    """returns a cipher where each letter in the plaintext is 'shifted' a certain number of places down the alphabet."""
+def caesar_cipher(text: str, shift: int):
+    """
+    Encrypts the given text using the Caesar cipher.
+
+    :param text: The text to encrypt.
+    :param shift: The number of positions to shift each letter by.
+    :return: The encrypted text.
+    """
+    if not isinstance(text, str):
+        raise ValueError("text must be a string")
+    if not isinstance(shift, int):
+        raise ValueError("shift must be an integer")
+
     # initialize an empty string for the result
     result = ""
 
@@ -10,7 +21,9 @@ def caesar_cipher(text: str, shift: int) -> str:
             # determine the unicode offset based on whether the letter is uppercase or lowercase
             unicode_offset = 65 if char.isupper() else 97
             # encrypt the letter
-            encrypted_char = chr((ord(char) + shift - unicode_offset) % 26 + unicode_offset)
+            encrypted_char = chr(
+                (ord(char) + shift - unicode_offset) % 26 + unicode_offset
+            )
             # append the encrypted char to the result
             result += encrypted_char
         else:
---
+++ rot13.py
@@ -0,0 +1,29 @@
+def rot13(text):
+    """
+    Encrypts the given text using the ROT13 cipher.
+
+    :param text: The text to encrypt.
+    :return: The encrypted text.
+    """
+
+    if not isinstance(text, str):
+        raise ValueError("text must be a string!")
+
+    result = ""
+
+    # loop over each character in the text
+    for char in text:
+        # check if the character is an alphaber letter
+        if char.isalpha():
+            # determine the unicode offset based on whether the letter is uppercase or lowercase
+            unicode_offset = 65 if char.isupper() else 97
+            # encrypt the letter
+            encrypted_letter = chr(
+                (ord(char) + 13 - unicode_offset) % 26 + unicode_offset
+            )
+            # append it to result
+            result += encrypted_letter
+        else:
+            # append it as is
+            result += char
+    return result
