1611e846c1060d718f5ab89243d01e62d8ccfce9
mustafakemal1729 <experimus1@gmail.com>
2023-06-02 23:20:40 +0200

    caesar cipher

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -11,9 +11,9 @@ def caesar_cipher(text: str, shift: int) -> str:
             unicode_offset = 65 if char.isupper() else 97
             # encrypt the letter
             encrypted_char = chr((ord(char) + shift - unicode_offset) % 26 + unicode_offset)
-
+            # append the encrypted char to the result
             result += encrypted_char
         else:
-            # append it to result
+            # append it as is
             result += char
     return result
